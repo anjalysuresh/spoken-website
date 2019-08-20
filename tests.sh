bbdee1537a756bc1b3d6d698fe91f47b8ddefa2a
anjali <anjalyusresh@cse.iitb.ac.in>
2019-08-20 16:07:44 +0530

    changed tests.sh

--- tests.sh
+++ tests.sh
@@ -2,6 +2,13 @@
 
 export WORKSPACE='var/lib/jenkins/workspace/venv/bin'
 
+if [ ! -d "venv" ]; then
+        virtualenv venv
+fi
+. venv/bin/activate
+
+
+
 pwd
 
 python3 manage.py test --keepdb scriptmanager
