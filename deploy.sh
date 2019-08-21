81c23f5e8764b5c12c4f7f013f47cf48ac69b898
anjali <anjalyusresh@cse.iitb.ac.in>
2019-08-21 15:25:00 +0530

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-ssh root@10.129.132.109
+USERNAME=anjali
+IP=10.129.132.109
+ssh -l ${USERNAME} ${IP} "pwd; ls -la"
+
+#ssh root@10.129.132.109
+pwd
 cd spoken_tutorial  
 virtualenv -p python3 venv
 if [ ! -d "venv" ]; then
@@ -8,35 +13,34 @@ fi
 . venv/bin/activate
 
 
-sudo rm -rf spoken-website
-git clone https://github.com/anjalysuresh/spoken-website.git
+#sudo rm -rf spoken-website
+#git clone https://github.com/anjalysuresh/spoken-website.git
 
 
 
 #sudo apt-get install python3-dev libmysqlclient-dev
 
-pwd
-pip3 install -r requirements-dev.txt
-pip3 install -r requirements-py3.txt
+#pip3 install -r requirements-dev.txt
+#pip3 install -r requirements-py3.txt
 
 
 
-#cd Spoken tutorial script creation 
+ 
 
-cp sample.config.py spoken/config.py
-cd spoken
+#cp sample.config.py spoken/config.py
+#cd spoken
 
-chown root:root config.py
-cd ..
-cd events
+#chown root:root config.py
+#cd ..
+#cd events
 
-cat > display.py
-chmod 777 display.py
-pwd
-cd ..
-pwd
+#cat > display.py
+#chmod 777 display.py
+#pwd
+#cd ..
+#pwd
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 
+#python3 manage.py makemigrations
+#python3 manage.py migrate
+#python3 manage.py runserver 0.0.0.0:8000 
 
